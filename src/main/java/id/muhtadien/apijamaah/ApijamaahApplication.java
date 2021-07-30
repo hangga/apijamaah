84440ee5922b3ff513d24dec56e6bfa0d8361fce
Hangga Aji Sayekti <bazeniancode@gmail.com>
2021-07-30 08:42:52 +0700

    [Add] Pertama

---
+++ src/main/java/id/muhtadien/apijamaah/ApijamaahApplication.java
@@ -0,0 +1,13 @@
+package id.muhtadien.apijamaah;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApijamaahApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApijamaahApplication.class, args);
+	}
+
+}
---
+++ src/test/java/id/muhtadien/apijamaah/ApijamaahApplicationTests.java
@@ -0,0 +1,13 @@
+package id.muhtadien.apijamaah;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApijamaahApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
